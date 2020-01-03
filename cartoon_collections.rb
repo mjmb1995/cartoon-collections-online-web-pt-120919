50e2eb5c7f8628baa2bc1d034e433e260637480c
Mark Bihag <markbihag330@gmail.com>
2020-01-03 03:56:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,9 @@
 def roll_call_dwarves(collection)# code an argument here
   # Your code here
-  collection.each do |name|
-    put name
+  i = 0
+  while i > collection.length
+    puts "#{i} #{collection[i]}"
+    i += 1
   end
 end
 
