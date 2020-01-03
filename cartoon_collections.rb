093b112bb3bfe651a476d973af6961a3107b93f5
Mark Bihag <markbihag330@gmail.com>
2020-01-03 04:09:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ def long_planeteer_calls(collection)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(collection)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  collection.detect {|i| i.includes?(cheese_types)}
 end
