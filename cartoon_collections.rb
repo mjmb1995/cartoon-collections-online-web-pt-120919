95be1f48f6c4e497fa89d0eebd87b5dd500f5d45
Mark Bihag <markbihag330@gmail.com>
2020-01-03 04:12:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,8 @@ end
 def find_the_cheese(collection)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  collection.detect {|i| i.include?(cheese_types)}
+  i = 0
+  while i < cheese_types.length
+  collection.include?(cheese_types[i])    
+  end
 end
